14e08cb8919457d40240d1f052c6c329d838c6ff
erenpehli11 <eren.pehli1@gmail.com>
2025-05-22 17:47:40 +0300

    g

--- MehmetHairDesigner.Server.Infrastructure/Persistence/AppDbContext.cs
+++ MehmetHairDesigner.Server.Infrastructure/Persistence/AppDbContext.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using MehmetHairDesigner.Server.Infrastructure.Entities;
+using MehmetHairDesigner.Server.Domain.Entities;
 using System;
 
 namespace MehmetHairDesigner.Server.Infrastructure.Persistence
@@ -11,6 +12,11 @@ namespace MehmetHairDesigner.Server.Infrastructure.Persistence
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+
+          public DbSet<Appointment> Appointments { get; set; }
+
+        // Berberler
+        public DbSet<Barber> Barbers { get; set; }
 
     }
 }
